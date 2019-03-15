253da0045ff913204a691d8396dc466b2e42e5e9
Christopher <christopher.ferreira.silva@outlook.com>
2019-03-14 22:20:04 -0300

    Implementado mais uma funcionalidade

---
+++ src/com/agenda/servlet/CadastraUsuarioServlet.java
@@ -0,0 +1,36 @@
+package com.agenda.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.agenda.model.Pessoa;
+
+public class CadastraUsuarioServlet extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Pessoa pessoa = new Pessoa();
+		
+		request.getParameter("name");
+		
+		pessoa.setNome(request.getParameter("name"));
+		
+		request.getParameter("telefone");
+		
+		pessoa.setTelefone(request.getParameter("telefone"));
+		
+		request.getParameter("email");
+		
+		pessoa.setEmail(request.getParameter("email"));
+		
+		request.getParameter("endereco");
+		
+		pessoa.setEndereco(request.getParameter("endereco"));
+		
+		
+	}
+
+}
