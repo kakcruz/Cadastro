9a62b1490117e9494c9ac9b18b10ff5846479254
kakcruz <karolinecsantos@hotmail.com>
2019-03-21 21:36:44 -0300

    Atualizei meu projeto Cadastro

--- src/com/agenda/service/CadastraUsuarioService.java
+++ src/com/agenda/service/CadastraUsuarioService.java
@@ -1,5 +1,17 @@
 package com.agenda.service;
 
+import com.agenda.model.Pessoa;
+
 public class CadastraUsuarioService {
+	
+	public static void cadastra (Pessoa pessoa) {
+		
+		System.out.println(pessoa.getNome());
+		System.out.println(pessoa.getEndereco());
+		System.out.println(pessoa.getTelefone());
+		System.out.println(pessoa.getEmail());
+		
+		
+	}
 
 }
--- src/com/agenda/servlet/CadastraUsuarioServlet.java
+++ src/com/agenda/servlet/CadastraUsuarioServlet.java
@@ -8,27 +8,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.agenda.model.Pessoa;
+import com.agenda.service.CadastraUsuarioService;
 
 public class CadastraUsuarioServlet extends HttpServlet {
 	@Override
+	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Pessoa pessoa = new Pessoa();
 		
 		request.getParameter("name");
-		
 		pessoa.setNome(request.getParameter("name"));
 		
 		request.getParameter("telefone");
-		
 		pessoa.setTelefone(request.getParameter("telefone"));
 		
 		request.getParameter("email");
-		
 		pessoa.setEmail(request.getParameter("email"));
 		
 		request.getParameter("endereco");
-		
 		pessoa.setEndereco(request.getParameter("endereco"));
+		
+		CadastraUsuarioService service = new CadastraUsuarioService();
+		
+		service.cadastra(pessoa);
 		
 		
 	}
