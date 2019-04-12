c705e50936a834387beab917a461dd9410076216
kakcruz <karolinecsantos@hotmail.com>
2019-04-11 21:23:41 -0300

    Atualizamos 11do04

--- src/com/agenda/dao/PessoaDAO.java
+++ src/com/agenda/dao/PessoaDAO.java
@@ -1,17 +1,37 @@
 package com.agenda.dao;
 
+import java.sql.SQLException;
+
 import com.agenda.model.Pessoa;
+import com.agenda.util.ConnectionFactory;
 import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
 
 public class PessoaDAO {
-	
-	private Connection connection; 
-	
-	public void cadastra (Pessoa pessoa) {
-		
-		String SQL ="insert into pessoas (nome, email, endereco, telefone) values(?,?,?,?)";
-		
-	}
-		
+
+	private Connection connection;
+
+	public void cadastra(Pessoa pessoa) {
+
+		String SQL = "insert into pessoas (nome, email, endereco, telefone) values(?,?,?,?)";
+
+		try {
+
+			this.connection = new ConnectionFactory().getConnection();
+			PreparedStatement stmt = (PreparedStatement) this.connection.prepareStatement(SQL);
+
+			stmt.setString(1, pessoa.getNome());
+			stmt.setString(2, pessoa.getEmail());
+			stmt.setString(3, pessoa.getEndereco());
+			stmt.setString(4, pessoa.getTelefone());
+
+			stmt.execute();
+			stmt.close();
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
 	}
 
+}
