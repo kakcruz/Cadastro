e57df07eaf61f949dabad91f1c0cb88f25d2ebc0
kakcruz <karolinecsantos@hotmail.com>
2019-04-11 22:07:48 -0300

    Criamos uma nova pagina html

---
+++ src/com/agenda/servlet/ListarContatoServlet.java
@@ -0,0 +1,11 @@
+package com.agenda.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ListarContatoServlet {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {}
+}
